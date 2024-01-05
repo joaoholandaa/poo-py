98de55de59bd2724733c78687127a76ad23e5aa7
Joao Holanda <joaocmdt505@gmail.com>
2024-01-05 16:32:32 -0300

    variáveis e métodos de classes

---
+++ aula04/loja.py
@@ -0,0 +1,43 @@
+'''
+------------------------------------
+| Loja                             |
+------------------------------------
+|+tarifa:int                       |
+|-endereço:str                     |
+------------------------------------
+|+apresentar_endereco:None         |
+|+vender:int                       |
+|+alterar_tarifa(nova_tarifa:int):None|
+---------------------------------------
+'''
+
+class Loja:
+
+  tarifa = 1.03
+
+  def __init__(self, endereco: str) -> None:
+    self.__endereco = endereco
+
+  def apresentar_endereco(self) -> None:
+    print(self.__endereco)
+
+  @classmethod
+  def vender(cls) -> int:
+    return 40 * cls.tarifa
+
+  @classmethod
+  def alterar_tarifa(cls, nova_tarifa: int) -> None:
+    cls.tarifa = nova_tarifa
+
+loja_praia = Loja('Praia')
+loja_centro = Loja('Centro')
+
+loja_praia.apresentar_endereco()
+
+print(loja_praia.vender())
+print(loja_centro.vender())
+
+loja_centro.alterar_tarifa(1.50)
+
+print(loja_praia.vender())
+print(loja_centro.vender())
