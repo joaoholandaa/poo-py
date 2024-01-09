7b3b77774fb4350e2532a545f8135cca64a94ca6
Joao Holanda <joaocmdt505@gmail.com>
2024-01-09 12:21:43 -0300

    métodos e classes abstratas

---
+++ aula15/run.py
@@ -0,0 +1,14 @@
+from abs_class import AbstractClass
+
+class Filha(AbstractClass):
+
+  def apresentar_metodo(self) -> None:
+    print(self.atributo)
+
+  def metodo_abstrato(self) -> None:
+    print('Implementando método abstrato')
+
+filha = Filha()
+filha.apresentar_metodo()
+filha.metodo('Estou aqui')
+filha.metodo_abstrato()
